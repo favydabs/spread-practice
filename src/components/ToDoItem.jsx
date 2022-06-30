c63ca656f20412aa923a6920219cb3a25a3ea61f
favydabs <favourdabit@gmail.com>
2022-06-30 11:18:55 +0100

    line-through

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function ToDoItem(props){
-    return <li>{props.text}</li>
+function ToDoItem(props) {
+    const [isDone, setisDone] = useState(false);
+
+    function handleClick() {
+        setisDone(prevValue => {
+            return !prevValue;
+        })
+    }
+    return (
+        <div onClick={handleClick}>
+            <li style={{ textDecoration: isDone ? "line-through" : "none" }}>{props.text}</li>
+
+        </div>
+    );
 }
 export default ToDoItem;
