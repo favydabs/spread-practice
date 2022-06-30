c7f8547953f88acdcdd901f8620aeaae49cb250a
favydabs <favourdabit@gmail.com>
2022-06-30 10:22:04 +0100

    first stage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  const [inputText, setinputText] = useState("");
+
+  function handleChange(event) {
+    const newValue = event.target.value
+    setinputText(newValue);
+  }
+
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
+        <input onChange={handleChange} type="text" value={inputText} />
         <button>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          <li> a item</li>
         </ul>
       </div>
     </div>
